df7d265fea2ddf40848a0192476deeaa18a92503
Mostafa <m-hgr@hotmail.com>
2021-01-29 11:30:10 -0800

    Implemented error handling

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, send_file
+from flask import render_template, send_file, flash
 from app import app
 from app.forms import TestForm
 import os.path
@@ -23,39 +23,30 @@ def index():
     rawdata = form.user_upload.data
     rawquery = form.user_upload_query.data
     if form.validate_on_submit() and allowed_upload(rawdata.filename):
-        # pfams = {1: ['PF06325', 'IPR025799', 'IPR029063'],
-        #          2: ['PF00355', 'IPR036922', 'IPR017941'],
-        #          3: ['PF13535', 'IPR011761'],
-        #          4: ['PF01425', 'IPR036928', 'IPR023631'],
-        #          5: ['PF07859'],
-        #          6: ['PF07690']}
-        #
-        # labels = {1: 'PrmA',
-        #           2: 'Reiske',
-        #           3: 'ATP-grasp',
-        #           4: 'Amidase',
-        #           5: 'AB_Hydrolase_3',
-        #           6: 'MFS'}
+        try:
+            labels, pfams = query_parser.csv(rawquery.filename)
 
-        labels, pfams = query_parser.csv(rawquery.filename)
+            qinput = cytotyper_test.query(pfams, labels)
 
-        qinput = cytotyper_test.query(pfams, labels)
+            cytotyper_test.gap_unit = form.user_min_gap_size.data
+            cytotyper_test.min_occurence = form.user_min_occurences.data
+            analysis = cytotyper_test.analysis(rawdata, qinput)
 
-        cytotyper_test.gap_unit = form.user_min_gap_size.data
-        cytotyper_test.min_occurence = form.user_min_occurences.data
-        analysis = cytotyper_test.analysis(rawdata, qinput)
+            report = bgc_report_generator.fill_template(query=analysis.query,
+                                                        genotypes=analysis.genotypes)
 
-        report = bgc_report_generator.fill_template(query=analysis.query,
-                                                    genotypes=analysis.genotypes)
+            bgc_report_generator.write_report(jinja_output=report,
+                                              filename='app/output/report.html')
 
-        bgc_report_generator.write_report(jinja_output=report,
-                                          filename='app/output/report.html')
-
-        analysis.cytotable.to_csv('app/output/cytoscape_keys.csv')
-        rawdata.save(os.path.join(app.config['UPLOAD_DIR'], rawdata.filename))
-        filewrap.rmfile('app/output.zip')
-        filewrap.zip_wrap('app/output')
-        return send_file('output.zip', attachment_filename='results.zip')
+            analysis.cytotable.to_csv('app/output/cytoscape_keys.csv')
+            rawdata.save(os.path.join(app.config['UPLOAD_DIR'], rawdata.filename))
+            filewrap.rmfile('app/output.zip')
+            filewrap.zip_wrap('app/output')
+            return send_file('output.zip', attachment_filename='results.zip')
+        except Exception as e:
+            print(e)
+            flash("Either something's wrong with your submission, or there's a bug!"
+                  f"Let Mostafa know about this. Tell him it's about '{e}'")
     return render_template('index.html',
                            title='Home',
                            form=form)
